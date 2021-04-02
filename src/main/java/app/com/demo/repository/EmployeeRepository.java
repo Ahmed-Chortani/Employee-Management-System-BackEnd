402cc7072414009062f4583f290b4974128a2665
ahmed <troysucks@hotmail.com>
2021-04-02 13:57:30 +0200

    first commit

---
+++ src/main/java/app/com/demo/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package app.com.demo.repository;
+
+import app.com.demo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
